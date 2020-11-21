bd5e8bf5fdc204eae9c4c6061bac255cf54ad919
Matteo Colucci <matitald@gmail.com>
2020-11-21 21:55:41 +0100

    Display expiration date

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,15 +2,19 @@
   <v-list-item>
     <v-list-item-content>
       <v-list-item-title>{{ product.name }}</v-list-item-title>
-      <v-list-item-subtitle>Expires {{ expiry }}</v-list-item-subtitle>
+      <v-list-item-subtitle :class="{ 'red--text': expired }">
+        {{ expiresLabel }} {{ expiry }}
+      </v-list-item-subtitle>
     </v-list-item-content>
   </v-list-item>
 </template>
 
 <script>
 import dayjs from 'dayjs';
+import 'dayjs/locale/it'
 import RelativeTime from 'dayjs/plugin/relativeTime'
 
+dayjs.locale('it');
 dayjs.extend(RelativeTime);
 
 export default {
@@ -19,9 +23,26 @@ export default {
   ],
 
   computed: {
+    expireDate: function() {
+      return dayjs(this.product.expireDate).startOf('day');
+    },
+
+    expired: function() {
+      return this.expireDate.isBefore(this.today());
+    },
+
+    expiresLabel: function() {
+      return this.expired ? 'Scaduto' : 'Scade';
+    },
     expiry: function() {
-      return dayjs().to(dayjs(this.product.expireDate))
+      return this.today().to(this.expireDate);
     }
+  },
+
+  methods: {
+    today: function() {
+      return dayjs().startOf('day');
+    },
   }
 }
 </script>
